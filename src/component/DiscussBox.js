6f300b6143b6ab6d8f20f2b08359024586f7c145
ragpanv <72090309+ragpanv@users.noreply.github.com>
2021-05-21 02:45:16 +0530

    Update DiscussBox.js

--- src/component/DiscussBox.js
+++ src/component/DiscussBox.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import '../css/DiscussBox.css'
 import {Avatar} from '@material-ui/core';
+import { useSelector } from "react-redux";
+import { selectUser } from '../features/userSlice';
+
 function DiscussBox(){
+    const user=useSelector(selectUser)
     return (
         <div className="discussBox">
             <div className="discussBox_info">
-                <Avatar/>
-                <h5>Username</h5>
+                <Avatar src={user.photo}/>
+                <h5>{user.displayName}</h5>
             </div>
             <div className="discussBox__disucss">
                 <p>What is your question?</p>
