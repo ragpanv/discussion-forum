1a03af591671a2f231568c9d5fcc5c538a61d7f5
ragpanv <72090309+ragpanv@users.noreply.github.com>
2021-05-21 02:55:08 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useEffect } from "react";
 import "./App.css";
 import Login from "./component/auth/Login";
-import Link from 'react-router-dom';
 import Discuss from "./component/Discuss";
+import { useDispatch, useSelector } from "react-redux";
+import { login, logout, selectUser } from "./features/userSlice";
+import { auth } from "./firebase";
 
 function App() {
+  const user=useSelector(selectUser)
+  const dispatch=useDispatch()
+
+  useEffect(()=>{
+    auth.onAuthStateChanged((authUser)=>{
+      if(authUser){
+        dispatch(login({
+          uid: authUser.uid,
+          photo: authUser.photoURL,
+          displayName: authUser.displayName,
+          email: authUser.email,
+        }));
+        
+            console.log(authUser);
+      }else{
+        dispatch(logout())
+      }
+    })
+  }, [dispatch])
+
   return (
   <div className="App">
-    <Discuss/>
+    {
+      user ? (<Discuss/>):(<Login/>)
+    }
 
   </div>
   );
