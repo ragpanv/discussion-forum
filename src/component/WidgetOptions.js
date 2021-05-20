a1effc98e69a8141d772f021da41d48b55ac13b8
ragpanv <72090309+ragpanv@users.noreply.github.com>
2021-05-21 02:49:32 +0530

    Update WidgetOptions.js

--- src/component/WidgetOptions.js
+++ src/component/WidgetOptions.js
@@ -3,65 +3,103 @@ import '../css/WidgetOptions.css'
 
 function WidgetOptions() {
   return (
-    <div className="widget__contents">
-      <div className="widget__content">
+    <div className="widget_contents">
+      <div className="widget_content">
         <img
           src="https://qphs.fs.quoracdn.net/main-thumb-ti-1737435-100-jxcfmjdvwvpkcketifttdmeeimxcatua.jpeg"
           alt=""
         />
-        <div className="widget__contentTitle">
+        <div className="widget_contentTitle">
           <h5>Mobile App Programmer</h5>
-          <p>The best Mobile App Development Company</p>
+          <a
+            target="_blank" rel="noreferrer"
+            href="https://buildfire.com/understanding-mobile-app-development-lifecycle/"
+          >
+            The best Mobile App Development Company
+          </a>
         </div>
       </div>
-      <div className="widget__content">
+
+      <div className="widget_content">
         <img
           src="https://qphs.fs.quoracdn.net/main-thumb-ti-1574818-100-mzdwostcualpwcxekyrvyqqpychetdoc.jpeg"
           alt=""
         />
-        <div className="widget__contentTitle">
+        <div className="widget_contentTitle">
           <h5>Quota of Quotes</h5>
-          <p>Daily dosage of unforgettable lines from ...</p>
+          <a target="_blank" rel="noreferrer"
+            href="https://www.brainyquote.com/topics/daily-quotes"
+          >
+            Daily dosage of unforgettable lines from ...
+          </a>
         </div>
       </div>
-      <div className="widget__content">
+
+      <div className="widget_content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1644613-100-ydflucgoeztbhwyurtmlqqrgfqmjmhpl.jpeg"
+          src="https://image.shutterstock.com/image-photo/web-development-word-cloud-related-260nw-244951207.jpg"
           alt=""
         />
-        <div className="widget__contentTitle">
-          <h5>Art & Artist</h5>
-          <p>A Space retated to creating, practicing an...</p>
+        <div className="widget_contentTitle">
+          <h5>Web Development</h5>
+          <a target="_blank" rel="noreferrer" href="https://www.w3schools.com/whatis/">
+            Trending topic in recent TECH...
+          </a>
         </div>
       </div>
-      <div className="widget__content">
+
+      <div className="widget_content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1647318-100-kmwvqbpzatmylibelrowrerfqbspekwo.jpeg"
+          src="https://image.shutterstock.com/image-photo/asian-programmer-woman-looking-on-260nw-1936761496.jpg"
           alt=""
         />
-        <div className="widget__contentTitle">
-          <h5>Friedrich Nietzche</h5>
-          <p>A Space dedicated to great work of Friedrich...</p>
+        <div className="widget_contentTitle">
+          <h5>Coding</h5>
+          <a target="_blank" rel="noreferrer" href="https://www.w3schools.com">
+            Coding for your journey...
+          </a>
         </div>
       </div>
-      <div className="widget__content">
+
+      <div className="widget_content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1578647-100-xkggvbyzfkvzhyklewtkjijefekqbazb.jpeg"
+          src="https://image.shutterstock.com/image-photo/machine-learning-concept-man-260nw-730498120.jpg"
           alt=""
         />
-        <div className="widget__contentTitle">
-          <h5>Stock Market Strategies</h5>
-          <p>Everything about investing in Stock...</p>
+        <div className="widget_contentTitle">
+          <h5>Machine Learning</h5>
+          <a
+            target="_blank" rel="noreferrer"
+            href="https://www.geeksforgeeks.org/machine-learning/"
+          >
+            Everything about should know about ML...
+          </a>
         </div>
       </div>
-      <div className="widget__content">
+
+      <div className="widget_content">
         <img
-          src="https://qphs.fs.quoracdn.net/main-thumb-ti-1711686-100-glvgnbatdxpjbhrkyphlfamqrryfccvp.jpeg"
+          src="https://image.shutterstock.com/image-photo/smiling-young-businesswoman-planner-standing-260nw-1906650343.jpg"
           alt=""
         />
-        <div className="widget__contentTitle">
-          <h5>Architecture World</h5>
-          <p>All about civil architecture...</p>
+        <div className="widget_contentTitle">
+          <h5>Entrepreneur</h5>
+          <a target="_blank" rel="noreferrer" href="https://www.entrepreneur.com/in">
+            Want to set a Company...
+          </a>
+        </div>
+      </div>
+
+      <div className="widget_content">
+        <img
+          src="https://image.shutterstock.com/image-photo/mountains-under-mist-morning-amazing-260nw-1725825019.jpg"
+          alt=""
+        />
+        <div className="widget_contentTitle">
+          <h5>Towards Nature our DUTY</h5>
+          <a target="_blank" rel="noreferrer" href="https://www.nature.org/en-us/">
+            Nature which nurishes us...
+          </a>
         </div>
       </div>
     </div>
