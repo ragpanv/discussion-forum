ecc53a24503ae604f497ab44437d8a095fee95cc
ragpanv <72090309+ragpanv@users.noreply.github.com>
2021-05-21 02:46:21 +0530

    Update Navbar.js

--- src/component/Navbar.js
+++ src/component/Navbar.js
@@ -1,25 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
 import HomeIcon from '@material-ui/icons/Home'
 import FeaturedPlayListOutlinedIcon from "@material-ui/icons/FeaturedPlayListOutlined";
 import AssignmentTurnedInOutlinedIcon from "@material-ui/icons/AssignmentTurnedInOutlined";
-import NotificationsOutlinedIcon from "@material-ui/icons/NotificationsOutlined";
 import PeopleAltOutlinedIcon from "@material-ui/icons/PeopleAltOutlined";
 import SearchIcon from "@material-ui/icons/Search";
-import { Avatar, Button } from '@material-ui/core';
+import { Avatar, Button,  Input } from '@material-ui/core';
 import LanguageIcon from "@material-ui/icons/Language"
 import "../css/Navbar.css"
+import { useSelector } from 'react-redux';
+import { selectUser } from '../features/userSlice';
+import db, {auth} from '../firebase'
+import firebase from 'firebase'
+import Modal from 'react-modal'
+import { ExitToApp, ExpandMore ,Link } from '@material-ui/icons';
 
 function Navbar(){
+  const user=useSelector(selectUser)
+  const [openModal, setOpenModal]=useState(false)
+  const [input, setInput]=useState("")
+  const [inputUrl, setInputUrl]=useState("")
+
+  const handleQuestion=(e)=>{
+    e.preventDefault()
+    setOpenModal(false)
+
+    db.collection("question").add({
+      question: input,
+      imageUrl: inputUrl,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+      user: user,
+      up: 0,
+      down: 0,
+    });
+
+    setInput('')
+    setInputUrl('')
+  };
+
     return (
       <div className="qHeader">
         <div className="Header_logo">
-          <img
+          <img 
             src="https://th.bing.com/th/id/OIP.-MxYaS4J1SySsb0msl-oMgHaDF?w=350&h=145&c=7&o=5&dpr=1.25&pid=1.7"
             alt=""
             width="100px"
           />
-          {/*https://www.google.com/imgres?imgurl=https%3A%2F%2Fknowmore.org.au%2Fwp-content%2Fuploads%2F2020%2F07%2Fknowmore-logo-social-1.jpg&imgrefurl=https%3A%2F%2Fknowmore.org.au%2Ffor-service-providers%2F&tbnid=zvIiYOLuXqZR2M&vet=10CHEQMyibAWoXChMI6OX12sS_7wIVAAAAAB0AAAAAEAI..i&docid=zfP4DVOHoHDHCM&w=842&h=596&q=know%20more&ved=0CHEQMyibAWoXChMI6OX12sS_7wIVAAAAAB0AAAAAEAI */}
-          {/*https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.logolynx.com%2Fimages%2Flogolynx%2F5f%2F5ff879c5deef9c4d714d2f00b2d9d8dc.jpeg&imgrefurl=https%3A%2F%2Fwww.logolynx.com%2Ftopic%2Fthe%2Bmore%2Byou%2Bknow&tbnid=odA0LR6NTjfERM&vet=12ahUKEwj80YK2xb_vAhUy3XMBHQD1AuAQMygHegUIARCkAQ..i&docid=5OxUNVlEhY5tEM&w=1008&h=1008&q=know%20more%20logo&ved=2ahUKEwj80YK2xb_vAhUy3XMBHQD1AuAQMygHegUIARCkAQ */}
         </div>
 
         {/*for icons */}
@@ -36,9 +61,6 @@ function Navbar(){
           <div className="qHeader_icon">
             <PeopleAltOutlinedIcon />
           </div>
-          <div className="qHeader_icon">
-            <NotificationsOutlinedIcon />
-          </div>
         </div>
 
         <div className="qHeader_input">
@@ -48,10 +70,79 @@ function Navbar(){
 
         <div className="qHeader_Rem">
           <div className="qHeader_avatar">
-            <Avatar />
+            <Avatar
+              src={
+                user.photo
+                  ? user.photo
+                  : "https://images.unsplash.com/photo-1552168324-d612d77725e3?ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8Y2FtZXJhfGVufDB8fDB8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60"
+              }
+            />
           </div>
-          <LanguageIcon />
-          <Button>Add Question</Button>
+          <ExitToApp onClick={() => auth.signOut()} />
+          <LanguageIcon onClick={()=> ("www.google.com")}/>
+          <Button onClick={() => setOpenModal(true)}>Add Question</Button>
+          <Modal
+            isOpen={openModal}
+            onRequestClose={() => setOpenModal(false)}
+            shouldCloseOnOverlayClick={false}
+            style={{
+              overlay: {
+                width: 700,
+                height: 600,
+                backgroundColor: "rgba(90 34 34 / 50%)",
+                zIndex: "1000",
+                top: "10%",
+                left: "20%",
+              },
+            }}
+          >
+            <div className="modal_title">
+              <h5>Add Question</h5>
+              <h5>Share Link</h5>
+            </div>
+            <div className="modal_info">
+              <Avatar
+                className="avatar"
+                src={
+                  user.photo
+                    ? user.photo
+                    : "https://images-platform.99static.com//_QXV_u2KU7-ihGjWZVHQb5d-yVM=/238x1326:821x1909/fit-in/500x500/99designs-contests-attachments/119/119362/attachment_119362573"
+                }
+              />
+              <p>{user.displayName ? user.displayName : user.mail} asked</p>
+              <div className="modal_scope">
+                <PeopleAltOutlinedIcon />
+                <p>Public</p>
+                <ExpandMore />
+              </div>
+            </div>
+            <div className="modal_field">
+              <Input
+                required
+                type="text"
+                value={input}
+                onChange={(e) => setInput(e.target.value)}
+                placeholder="Start your question with WH words etc..."
+              />
+              <div className="modal_fieldLink">
+                <Link />
+                <input
+                  value={inputUrl}
+                  onChange={(e) => setInputUrl(e.target.value)}
+                  type="text"
+                  placeholder="Optional: include a link that gives context"
+                ></input>
+              </div>
+            </div>
+            <div className="modal_buttons">
+              <button className="cancel" onClick={() => setOpenModal(false)}>
+                Close
+              </button>
+              <button onClick={handleQuestion} className="add">
+                Add Question
+              </button>
+            </div>
+          </Modal>
         </div>
       </div>
     );
